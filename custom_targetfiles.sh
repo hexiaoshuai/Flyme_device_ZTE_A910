380b69b5f9b5989dbff910b4e2aeeb7ad1fd1e04
贺帅 <heshuai@meizu.com>
2016-12-12 19:47:40 +0800

    升级至 5.1.12.9

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -6,7 +6,6 @@ TARGET_FILES_DIR=$OUT_DIR/merged_target_files
 OVERLAY_DIR=$PWD/overlay
 cp -a launcher.xml $TARGET_FILES_DIR/SYSTEM/media/
 cp -a plugin $TARGET_FILES_DIR/SYSTEM/
-cp -a bootanimation.zip $TARGET_FILES_DIR/SYSTEM/media/
 
 sed -i 's#ro.mtk_audio_profiles=1#ro.mtk_audio_profiles=0#g' $TARGET_FILES_DIR/SYSTEM/build.prop
 
